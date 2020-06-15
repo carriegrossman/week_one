e80db7dff4f324c6f5445b1c97ba23d02f92a07f
Carrie Grossman <carriegrossman@Carries-MacBook-Pro.local>
2020-06-15 10:00:32 -0400

    modified and added files

---
+++ week_one/mapping.py
@@ -0,0 +1,54 @@
+# 1
+# phonebook_dict = {
+#     'Alice': '703-493-1834',
+#     'Bob': '857-384-1234',
+#     'Elizabeth': '484-584-2923'
+# }
+# print(phonebook_dict['Elizabeth'])
+# del (phonebook_dict["Alice"])
+
+# ph1 = {'Bob': '968-345-2345'}
+# phonebook_dict.update(ph1)
+# print(phonebook_dict)
+
+# 2
+# ramit = {
+#   'name': 'Ramit',
+#   'email': 'ramit@gmail.com',
+#   'interests': ['movies', 'tennis'],
+#   'friends': [
+#     {
+#       'name': 'Jasmine',
+#       'email': 'jasmine@yahoo.com',
+#       'interests': ['photography', 'tennis']
+#     },
+#     {
+#       'name': 'Jan',
+#       'email': 'jan@hotmail.com',
+#       'interests': ['movies', 'tv']
+#     }
+#   ]
+# }
+# print(ramit['email'])
+# print(ramit['interests'][0])
+# print(ramit['friends'][0]['email'])
+# print(ramit['friends'][1]['interests'][1])
+
+#3
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ],
+}
